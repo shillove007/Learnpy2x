2a13b177cbaf5a87dcd5bb6a4f0971e667b1c213
Sushil-PC <katesushil@gamil.com>
2024-02-05 22:53:13 +0530

    initiate commit

---
+++ 05_02_2024/Lab012.py
@@ -0,0 +1,14 @@
+# Take the 2 number from the user and we want to add them
+
+# step1 = Take input
+# step2 = Sum
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+print(type(num1))
+
+# num3 = int(num1) + int(num2)
+num3 = num1 + num2
+
+print('Sum of two number=', num3)
+print(type(num3))
