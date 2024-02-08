a600628570b4297d65c8e669ff4bf31497f69150
Sushil-PC <katesushil@gamil.com>
2024-02-08 22:02:01 +0530

    initiate commit

---
+++ 07_02_2024/Task_2.py
@@ -0,0 +1,14 @@
+#   Create a program that takes two numbers as input and prints whether
+#   the first number is greater than, less than, or equal to the second number.
+
+#   takes two inputs from user.
+first_number = int(input("Enter first number "))
+second_number = int(input("Enter second number "))
+
+print("##############################################\n--------Result are Here--------\n")
+
+# Result are print after comparison
+print("first number greater than second number = ", first_number > second_number)
+print("first number less than second number = ", first_number < second_number)
+print("first number equal second number = ", first_number == second_number)
+4
---
+++ 07_02_2024/Task_3.py
@@ -0,0 +1,8 @@
+# Develop a Python script that calculates the square and cube of a given number.
+
+#   Take input from user
+number = int(input("Enter the number "))
+
+#   print square and cube of given number
+print("the square of a given number = ", (number ** 2))
+print("the cube of a given number = ", (number ** 3))
